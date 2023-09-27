a1897641376b938a31c8575a6c78e447ecce09e2
CN-CJJ <141293386+CN-CJJ@users.noreply.github.com>
2023-09-27 17:41:58 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,53 @@
+// JavaScript Document
+
+
+
+
+
+//控制台
+let styleTitle1 = `
+font-size: 20px;
+font-weight: 600;
+color: rgb(244,167,89);
+`;
+let styleTitle2 = `
+font-size:12px;
+color: rgb(244,167,89);
+`;
+let styleContent = `
+color: rgb(30,152,255);
+`;
+let title1 = "啊杰不会踢球";
+let title2 = `
+*********************************************** 
+┆  ┏┓　　　┏┓ ┆ 
+┆┏┛┻━━━┛┻┓ ┆ 
+┆┃　　　　　　　┃ ┆ 
+┆┃　　　━　　　┃ ┆ 
+┆┃　┳┛　┗┳　┃ ┆ 
+┆┃　　　　　　　┃ ┆ 
+┆┃　　　┻　　　┃ ┆ 
+┆┗━┓　  　┏━┛ ┆ 
+┆　　┃　  　┃ 　　┆　　　　　　 
+┆　　┃　  　┗━━━┓ ┆ 
+┆　　┃　    　　　　┣┓┆ 
+┆　　┃　        　　┏┛┆ 
+┆　　┗┓┓┏━┳┓┏┛ ┆ 
+┆　　　┃┫┫　┃┫┫ ┆ 
+┆　　　┗┻┛　┗┻┛ ┆ 
+************************************************                                       
+`;
+let content = `
+版 本 号：3.4
+更新日期：2022-07-24
+
+主页:  https://cn-cjj.github.io/
+`;
+console.error('小老弟还会按F12呢');
+console.error(
+  `%c${title1} %c${title2}
+	%c${content}`,
+  styleTitle1,
+  styleTitle2,
+  styleContent
+);
